a7f5e1f2db71921dc9fc1447091ab5987e360030
ThaummyG <gearwcr58@gmail.com>
2024-01-17 19:46:19 +0700

    test Exercise

--- JS.js
+++ JS.js
@@ -1,3 +1,30 @@
-const whisper = "              message                ";
+//Exercise 1
 
-whisper.toUpperCase().trim();
+let numKids = "2";
+
+let partner = "Deer";
+
+let geoLocation = "Chiang Mai";
+
+let jobTitle = "Dev";
+
+`You will be a ${jobTitle} in ${geoLocation}, and married to ${partner} with ${numKids} kids.`
+
+//Exercise 2
+
+let birthYear = "1996";
+
+let futureYear = "2024";
+
+`I will be either ${futureYear - birthYear} or ${futureYear - birthYear + 1} in ${futureYear}.`
+
+//Exercise 3
+let age = 27;
+
+let maxAge = 70;
+
+let numPerDay = 300;
+
+totalNeeded = maxAge - age;
+
+`You will need ${(totalNeeded * 365) * numPerDay} to last you until the ripe old age of ${maxAge}.`
