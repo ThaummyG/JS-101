2772b6cface02c8cd8052ad743618fc510ee64e1
ThaummyG <gearwcr58@gmail.com>
2024-01-17 20:02:47 +0700

    test Exercise edit2

--- JS.js
+++ JS.js
@@ -8,7 +8,7 @@ let geoLocation = "Chiang Mai";
 
 let jobTitle = "Dev";
 
-`You will be a ${jobTitle} in ${geoLocation}, and married to ${partner} with ${numKids} kids.`
+console.log(`You will be a ${jobTitle} in ${geoLocation}, and married to ${partner} with ${numKids} kids.`);
 
 //Exercise 2
 
@@ -16,7 +16,7 @@ let birthYear = 1996;
 
 let futureYear = 2024;
 
-`I will be either ${futureYear - birthYear} or ${futureYear - birthYear + 1} in ${futureYear}.`
+console.log(`I will be either ${futureYear - birthYear} or ${futureYear - birthYear + 1} in ${futureYear}.`);
 
 //Exercise 3
 let age = 27;
@@ -27,4 +27,4 @@ let numPerDay = 300;
 
 let totalNeeded = maxAge - age;
 
-`You will need ${(totalNeeded * 365) * numPerDay} to last you until the ripe old age of ${maxAge}.`
+console.log(`You will need ${(totalNeeded * 365) * numPerDay} to last you until the ripe old age of ${maxAge}.`);
