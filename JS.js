56a2874df9c12b7ef051a1285014a5402b17a187
ThaummyG <gearwcr58@gmail.com>
2024-01-21 18:02:09 +0700

    learn

--- JS.js
+++ JS.js
@@ -1,7 +1,39 @@
-let rating = 4;
+// let dayOfweek = prompt("pls enter today");
+// dayOfweek = dayOfweek.toLowerCase()
 
-if (rating >= 3) {
-    console.log("u r supperstar")
+// if (dayOfweek == "monday") {
+//     console.log("Today is Monday");
+// } else if (dayOfweek == "sunday") {
+//     console.log("Today is Sunday");
+// } else if (dayOfweek == "friday") {
+//     console.log("Today is Friday");
+// } else {
+//     console.log("Other day");
+// }
+
+// const type = "studen"
+
+// if (type == 'studen') {
+//     console.log("Free");
+// } else if (type == "teacher") {
+//     console.log("discount 40%");
+// } else if (type == "baby") {
+//     console.log("discount 90%");
+// } else {
+//     console.log("type invalid");
+// }
+
+let input = prompt("pls enter somenumbar")
+
+let random = Math.random()
+
+let number = Math.floor(random * 10)
+console.log(number)
+
+if (input < number) {
+    console.log("น้อยเกินไปละ");
+} else if (input > number) {
+    console.log("เยอะไปนะ");
+} else {
+    console.log("กว่าจะได้เนอะ");
 }
-
-console.log("u r not supperstar");
